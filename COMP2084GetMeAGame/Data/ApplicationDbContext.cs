47695d4b7895b28d572e87103e3793b9e2c14af9
ifotn <rich@infrontofthenet.com>
2020-10-05 14:00:50 -0400

    l4 - db connection w/scaffold controllers and views

--- COMP2084GetMeAGame/Data/ApplicationDbContext.cs
+++ COMP2084GetMeAGame/Data/ApplicationDbContext.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using COMP2084GetMeAGame.Models;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
@@ -8,9 +9,18 @@ namespace COMP2084GetMeAGame.Data
 {
     public class ApplicationDbContext : IdentityDbContext
     {
+        // reference each Model 
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Product> Products { get; set; }
+
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
             : base(options)
         {
+        }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
         }
     }
 }
--- COMP2084GetMeAGame/Data/Migrations/20201005173604_AddProductsWithCategoriesRelationship.Designer.cs
+++ COMP2084GetMeAGame/Data/Migrations/20201005173604_AddProductsWithCategoriesRelationship.Designer.cs
@@ -4,14 +4,16 @@ using COMP2084GetMeAGame.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace COMP2084GetMeAGame.Data.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20201005173604_AddProductsWithCategoriesRelationship")]
+    partial class AddProductsWithCategoriesRelationship
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
