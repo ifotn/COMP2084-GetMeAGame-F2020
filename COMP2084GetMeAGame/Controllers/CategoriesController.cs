0d5ff1dbe334a81fd097914fb0dfd836261fb8d2
ifotn <rich@infrontofthenet.com>
2020-10-19 11:57:53 -0400

    l6/1 - basic auth w/.net identity

--- COMP2084GetMeAGame/Controllers/CategoriesController.cs
+++ COMP2084GetMeAGame/Controllers/CategoriesController.cs
@@ -7,9 +7,11 @@ using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 using COMP2084GetMeAGame.Data;
 using COMP2084GetMeAGame.Models;
+using Microsoft.AspNetCore.Authorization;
 
 namespace COMP2084GetMeAGame.Controllers
 {
+    [Authorize]
     public class CategoriesController : Controller
     {
         private readonly ApplicationDbContext _context;
@@ -26,6 +28,7 @@ namespace COMP2084GetMeAGame.Controllers
         }
 
         // GET: Categories/Details/5
+        [AllowAnonymous]
         public async Task<IActionResult> Details(int? id)
         {
             if (id == null)
